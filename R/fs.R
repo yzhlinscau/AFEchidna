33b5b2888502b7c34287cf705c205c8ce200fe05
Yuanzhen Lin <yzhlinscau@163.com>
2017-04-26 13:08:41 +0800

    Add files via upload

---
+++ R/fs.R
@@ -0,0 +1,20 @@
+#' @title full-sib dataset
+#' 
+#' @description full-sib dataset
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name fs
+#' @usage fs
+#' @format one dataset with 8 variables
+#' \describe{
+#'  \item{TreeID} {Tree Sample code}
+#'  \item{Mum} {Parent Mum code}
+#'  \item{Dad} {Parent Dad code}
+#'  \item{Fam} {Family code}
+#'  \item{Fam2} {Family code2}
+#'  \item{Rep} {Replication code}
+#'  \item{Plot} {Plot code}
+#'  \item{dbh10} {Tree DBH at 10th year}
+#' }
+NULL
