38c0a7627f587d87da9d85a8fb224bc00b242549
Yuanzhen Lin <yzhlinscau@163.com>
2017-04-29 15:17:26 +0800

    Add files via upload

---
+++ R/zinc.R
@@ -0,0 +1,15 @@
+#' @title zinc dataset
+#' 
+#' @description Example dataset--zinc
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name zinc
+#' @usage zinc
+#' @format one dataset with 3 variables
+#' \describe{
+#'  \item{Family}{Family code}
+#'  \item{SeedZn}{Seed Zinc content}
+#'  \item{LeafZn}{Leaf Zinc content}
+#' }
+NULL
